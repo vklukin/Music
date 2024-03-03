6ed4801030869c30542b19ed7fa540cbc4f11c44
VK-Azeria <vk.azeria@gmail.com>
2024-03-03 10:30:53 +0500

    removed logging for isTrackPlaying state

--- src/core/contexts/PlayerContext/PlayerContext.hooks.ts
+++ src/core/contexts/PlayerContext/PlayerContext.hooks.ts
@@ -75,8 +75,6 @@ export const usePlayerContextHooks = ({
     }, []);
 
     useEffect(() => {
-        console.log(isTrackPlaying);
-
         if (isTrackPlaying) {
             audio.play();
         } else {
